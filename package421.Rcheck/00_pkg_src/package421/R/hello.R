3d7992fe99e7f0a69592bc511a5bd20791c2910c
cbettler <carlee.bettler@gmail.com>
2019-11-20 13:12:20 -0600

    another commit

---
+++ package421.Rcheck/00_pkg_src/package421/R/hello.R
@@ -0,0 +1,24 @@
+#' @title Test
+#'
+#' @description
+#'
+#' @param
+#'
+#' @return
+#'
+#' @examples
+#'
+#' @export
+
+library("stringi")
+
+randomLetterFunc <- function(n){
+  alphabetVec <- c()
+
+  alphabetVec <- stri_rand_strings(n, 1, '[A-Z]')
+
+  return(alphabetVec)
+}
+
+
+
